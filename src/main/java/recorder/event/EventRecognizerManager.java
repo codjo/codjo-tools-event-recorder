72c4997fbe3164b6aaaa41a05263a25ffb996849
gonnot <gonnot@yahoo.com>
2012-09-01 15:52:38 +0200

    Batch update of the copyright notice (Apache License 2.0)

--- src/main/java/recorder/event/EventRecognizerManager.java
+++ src/main/java/recorder/event/EventRecognizerManager.java
@@ -1,7 +1,20 @@
 /*
- * codjo.net
+ * codjo (Prototype)
+ * =================
  *
- * Common Apache License 2.0
+ *    Copyright (C) 2005, 2012 by codjo.net
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *    implied. See the License for the specific language governing permissions
+ *    and limitations under the License.
  */
 package recorder.event;
 import java.awt.*;
@@ -16,7 +29,7 @@ import javax.swing.tree.TreePath;
 import recorder.component.GuiComponent;
 import recorder.component.GuiComponentFactory;
 /**
- * Transform AWT event into {@link GuiEvent}.
+ * Transform AWT event into {@link recorder.event.GuiEvent}.
  */
 public class EventRecognizerManager {
     private GuiComponentFactory factory;
--- src/main/java/recorder/gui/action/SwingActionAdapter.java
+++ src/main/java/recorder/gui/action/SwingActionAdapter.java
@@ -1,7 +1,20 @@
 /*
- * codjo.net
+ * codjo (Prototype)
+ * =================
  *
- * Common Apache License 2.0
+ *    Copyright (C) 2005, 2012 by codjo.net
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *    implied. See the License for the specific language governing permissions
+ *    and limitations under the License.
  */
 package recorder.gui.action;
 import java.awt.event.ActionEvent;
@@ -10,6 +23,7 @@ import java.beans.PropertyChangeListener;
 import javax.swing.*;
 import org.apache.log4j.Logger;
 import recorder.gui.icons.IconsManager;
+import recorder.gui.icons.IconsManager.IconId;
 
 /**
  */
@@ -23,7 +37,7 @@ class SwingActionAdapter implements Action, PropertyChangeListener {
         if (null != guiAction.getValue(GuiAction.ICON_ID)) {
             guiAction.putValue(Action.SMALL_ICON,
                                IconsManager.getIcon(
-                                     (IconsManager.IconId)guiAction.getValue(GuiAction.ICON_ID)));
+                                     (IconId)guiAction.getValue(GuiAction.ICON_ID)));
         }
         if (null != guiAction.getValue(GuiAction.LABEL)) {
             guiAction.putValue(Action.NAME, guiAction.getValue(GuiAction.LABEL));
@@ -74,7 +88,7 @@ class SwingActionAdapter implements Action, PropertyChangeListener {
     public void propertyChange(PropertyChangeEvent evt) {
         if (GuiAction.ICON_ID.equals(evt.getPropertyName())) {
             putValue(Action.SMALL_ICON,
-                     IconsManager.getIcon((IconsManager.IconId)getValue(GuiAction.ICON_ID)));
+                     IconsManager.getIcon((IconId)getValue(GuiAction.ICON_ID)));
         }
         else if (GuiAction.TOOLTIP.equals(evt.getPropertyName())) {
             putValue(Action.SHORT_DESCRIPTION, getValue(GuiAction.TOOLTIP));
